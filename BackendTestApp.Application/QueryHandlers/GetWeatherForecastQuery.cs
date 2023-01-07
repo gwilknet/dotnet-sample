36dcec178e5926936616e2726598502e18fb3c2d
GW <contact@gwilk.net>
2023-01-07 16:03:11 +0100

    Add project files.

---
+++ BackendTestApp.Application/QueryHandlers/GetWeatherForecastQuery.cs
@@ -0,0 +1,34 @@
+﻿using BackendTestApp.Application.Model;
+
+namespace BackendTestApp.Application.QueryHandlers;
+
+public interface IQuery<TResult>
+{
+    TResult Handle();
+}
+
+public class GetWeatherForecastResult
+{
+    public IEnumerable<WeatherForecast>? ResultList { get; set; }
+}
+
+public class GetWeatherForecastQuery : IQuery<GetWeatherForecastResult>
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public GetWeatherForecastResult Handle()
+    {
+        return new GetWeatherForecastResult
+        {
+            ResultList = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray()
+        };
+    }
+}
---
+++ BackendTestApp/Program.cs
@@ -0,0 +1,32 @@
+using BackendTestApp.Application.QueryHandlers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IQuery<GetWeatherForecastResult>, GetWeatherForecastQuery>();
+
+
+//builder.Services.AddSingleton<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddTransient<>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
